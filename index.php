0931dc8c45459bf1d30b9129515747e65201a136
sun bo <sunbohouse@gmail.com>
2022-06-16 21:04:48 +0100

    DI Container Created

--- index.php
+++ index.php
@@ -1,11 +1,51 @@
 <?php
 
-use app\App;
+use app\Container;
+use app\EmailService;
 use app\InvoiceService;
+use app\PaymentGatewayService;
+use app\SalesTaxService;
 
 include __DIR__.'/vendor/autoload.php';
 
 
-$app = new App();
+// $app = new App();
 
-App::$container->get(InvoiceService::class)->process(['name'=>'Geo'], 25);
+// App::$container->get(InvoiceService::class)->process(['name'=>'Geo'], 25);
+
+//refactor Container
+$container = new Container();
+$container->set(SalesTaxService::class, fn()=>new SalesTaxService());
+$container->set(PaymentGatewayService::class, fn()=> new PaymentGatewayService);
+$container->set(EmailService::class, fn()=>new EmailService);
+
+$container->get(InvoiceService::class)->process(['name'=>'Geo'], 25);
+// (new Container())->get(InvoiceService::class)->process(['name'=>'Geo'], 25);
+
+// $className = 'app\InvoiceService';
+// $refClass = new ReflectionClass($className);
+
+// $constructor = $refClass->getConstructor();
+// $constructorParams = $constructor->getParameters();
+
+// var_dump($constructor);
+// var_dump($constructorParams);
+ 
+// $dependencies = array_map(function(ReflectionParameter $params) use($className){
+//     echo 'Params'.PHP_EOL;
+//     var_dump($params);
+//     $name = $params->getName();
+//     $type = $params->getType();
+    
+//     echo 'Name'.PHP_EOL;
+//     var_dump($name);
+
+//     echo 'Type'.PHP_EOL;
+//     var_dump($type);
+//     echo 'Type->getName()'.PHP_EOL;
+//     var_dump($type->getName());
+//     echo 'ClassName'.PHP_EOL;
+//     var_dump($className);
+//     echo '-----------'.PHP_EOL;
+
+// }, $constructorParams); 
--- src/app/Container.php
+++ src/app/Container.php
@@ -2,8 +2,14 @@
 
 namespace app;
 
-use app\Exceptions\Container\NotFoundException;
+use app\Exceptions\Container\ContainerException;
+use Exception;
 use Psr\Container\ContainerInterface;
+use ReflectionClass;
+use ReflectionNamedType;
+use ReflectionParameter;
+use ReflectionType;
+use ReflectionUnionType;
 
 class Container implements ContainerInterface
 {
@@ -13,16 +19,28 @@ class Container implements ContainerInterface
 
     public function get(string $id)
     {
-        if(!$this->has($id)){
-            throw new NotFoundException('class "'.$id.'" has no binding');
+
+        /**
+         * deprecated by throw exception, 
+         * if key found, return by calling the callable function
+         */
+        // if(!$this->has($id)){
+        //     throw new NotFoundException('class "'.$id.'" has no binding');
+        // }
+
+
+        if ($this->has($id)) {
+            $entry = $this->entires[$id];
+            echo '-------_HAS_______-' . PHP_EOL;
+            var_dump($entry);
+            return $entry($this);
         }
 
-        $entry = $this->entires[$id];
-
-        var_dump($entry);
-
-        return $entry($this);
+        $this->resolve($id);
     }
+
+
+
 
     public function has(string $id): bool
     {
@@ -37,5 +55,68 @@ class Container implements ContainerInterface
     public function test()
     {
         var_dump($this->entires);
+    }
+
+    public function resolve(string $id)
+    {
+        // 1. Inspect the class that we are trying to get from the container
+        $refectionClass = new ReflectionClass($id);
+
+        if (!$refectionClass->isInstantiable()) {
+            throw new ContainerException('Class ' . $id . ' is not instantiable');
+        }
+
+        // 2. inspect the constructor of the class
+
+        $constructor = $refectionClass->getConstructor();
+
+        //if there is no constructor for the class, new the class, then return
+        if (!$constructor) {
+            return new $id;
+        }
+
+        // 3. inspect the constructor parameters(Dependencies)
+
+        $parameters = $constructor->getParameters();
+
+        //if the constructor has no params, new the class, then return
+        if (!$parameters) {
+            return new $id;
+        }
+
+        var_dump($parameters);
+
+        // 4. if the constructor parameters is a class 
+        //then try to resolve the class using the container
+
+        $dependencies = array_map(
+            function (ReflectionParameter $params) use ($id) {
+                $name = $params->getName();
+                $type = $params->getType();
+
+                if (!$type) {
+                    throw new ContainerException('Failed to resolve class "' . $id . '" because param "' . $name . '" is missing type hint');
+                }
+
+                if ($type instanceof ReflectionUnionType) {
+                    throw new ContainerException('Failed to resolve class "' . $id . '" because of union type for params "' . $name . '"');
+                }
+
+                if ($type instanceof ReflectionNamedType && ! $type->isBuiltin()) {
+
+                    echo 'LLL' . PHP_EOL;
+                    echo $type->getName() . PHP_EOL;
+                    return $this->get($type->getName());
+                }
+
+                throw new ContainerException('Failed to resolve class "' . $id . '" because of the invalid params "' . $name . '"');
+            },
+            $parameters
+        );
+
+        var_dump($dependencies);
+        die('CCEFG');
+
+        return $refectionClass->newInstanceArgs($dependencies);
     }
 }
