e951589708ada8fb9810451418a830483598a233
sun bo <sunbohouse@gmail.com>
2022-06-18 22:03:41 +0100

    init

--- index.php
+++ index.php
@@ -1,28 +1,4 @@
 <?php
 
-use lib\ArrayUnpacking;
-use lib\ClassIterator\Invoice;
-use lib\Customer;
-use lib\FinalConstant\InvoiceQuery;
-use lib\NewCustomer;
 
 include "./vendor/autoload.php";
-
-$unpacking = new ArrayUnpacking();
-
-$unpacking->OldUnpacking();
-
-$unpacking->NewUnpacking();
-
-echo 'New Keywods Initializer'.PHP_EOL;
-
-$customer = New Customer();
-var_dump($customer);
-
-$newCustomer = new NewCustomer();
-var_dump($newCustomer);
-
-
-echo 'Final Const'.PHP_EOL;
-
-echo InvoiceQuery::DEFAULT_LIMIT.PHP_EOL;
--- src/lib/FinalConstant/TableQuery.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace lib\FinalConstant;
-
-class TableQuery{
-    public const DEFAULT_LIMIT = 25;
-    final public const  FINAL_DEFAULT_LIMIT = 20;
-}
